e0b2101160a4dad3d64723469f27c2ad82266baf
Joni Sadler <joni.anne.sadler@gmail.com>
2021-05-25 20:45:13 -0400

    Created restaurant.js and added basic structure

---
+++ frontend/src/services/restaurant.js
@@ -0,0 +1,29 @@
+import http from "../http-common";
+
+class RestaurantDataService {
+  getAll(page = 0) {
+    return http.get(`?page=${page}`);
+  }
+
+  get(id) {
+    return http.get(`/id/${id}`);
+  }
+
+  find(query, by = "name", page = 0) {
+    return http.get(`?${by}=${query}&page=${page}`);
+  }
+
+  updateReview(data) {
+    return http.put("/review", data);
+  }
+
+  deleteReview(id) {
+    return http.delete(`/review?id=${id}`);
+  }
+
+  getCuisines(id) {
+    return http.get(`/cuisines`);
+  }
+}
+
+export default new RestaurantDataService();
